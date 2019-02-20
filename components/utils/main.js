a5cb319c957ed1d4adbac9585c51cd14d76276ef
ljylhm <948021695@qq.com>
2019-02-20 14:15:17 +0800

    #修改防抖函数 #增加节流函数

--- components/utils/main.js
+++ components/utils/main.js
@@ -3,14 +3,38 @@ const utils = {
 
     /** 
      * @description 基础版防抖函数
+     * @param {fn | Function} 执行的函数 
+     * @param {timeout | Number} 设置的hash值 
      * @summary 重点在于闭包函数 公用内部的一个变量
     */
-    debounce(fn, delay = 200, retryTimes = 3) {
-        // 定义一个时间
-        let timeout = "";
+    debounce(fn, timeout = 500) {
+        let timer
         return function () {
-            if (timeout) clearTimeout(timeout);
-            timeout = setTimeout(fn, delay);
+            let env = this
+            clearTimeout(timer)
+            timer = setTimeout(function () {
+                fn.apply(env, arguments);
+            }, timeout)
+        }
+    },
+
+    /** 
+     * @description 节流函数
+     * @param {fn | Function} 执行的函数 
+     * @param {timeout | Number} 设置的hash值 
+     * @returns {void}
+     * @summary 针对高频事件 在固定的时间内执行
+    */
+    throttle(fn, timeout = 500) {
+        let canRun = true
+        return function () {
+            let env = this
+            if (!canRun) return
+            canRun = false
+            setTimeout(function () {
+                fn.apply(env, arguments)
+                canRun = true
+            }, timeout)
         }
     }
 }
