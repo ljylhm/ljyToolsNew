9036cbd98d9a95c219091c53e3bb3b37dd81d7fd
ljylhm <948021695@qq.com>
2019-02-25 14:50:36 +0800

    # 增加删除数组中某个索引的方法 #以后添加方法需在前面加上时间

--- components/array/main.js
+++ components/array/main.js
@@ -1,9 +1,10 @@
+import verify from "@components/verify/main.js";
 /**
-     * @description 清除数组中重复的对象
-     * @return {Object} 一个新的数组
-     * @example let c = [{id:1,value:2},{id:2,value:2},{id:2,value:3}] 
-     * => [{id:1,value:2},{id:2,value:2}]
-     */
+ * @description 清除数组中重复的对象
+ * @return {Object} 一个新的数组
+ * @example let c = [{id:1,value:2},{id:2,value:2},{id:2,value:3}] 
+ * => [{id:1,value:2},{id:2,value:2}]
+ */
 export function clearRepeat(arr, key) {
     var o = {};
     return arr.reduce((prev, cur, index, arr) => {
@@ -13,4 +14,19 @@ export function clearRepeat(arr, key) {
         }
         return prev;
     }, [])
+}
+
+// created by ljy 2019/02/25
+/**
+ * @description 删除数组中的某个元素 
+ * @param {arr | Array} 要删除的数组
+ * @param {index | (Number | String)} 数组索引
+ * @return {Array} 一个新的数组
+ */
+export function cut(arr, index) {
+    if (arguments.length < 2 || verify.isArray(arr)) {
+        return new Array()
+    }
+    arr.splice(index, 1)
+    return arr
 }
