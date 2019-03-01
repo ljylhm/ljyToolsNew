0daf15886f44895398a5e14ff17ed6974b333412
ljylhm <948021695@qq.com>
2019-03-01 17:59:50 +0800

    # 暂时提交

---
+++ components/interview/main.js
@@ -0,0 +1,13 @@
+// 面试常用js代码搜集
+export const DataStructure = {
+    bubleSort(data = []) {
+        let len = data.length
+        let public = null
+        for (let i = 0; i < len; i++) {
+            for (let j = i; j < len; j++) {
+                public = data[j]
+            }
+        }
+    }
+}
+
--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 let t = require("./dist/main");
 module.exports = t;
 
-var x = {
-    a: 321,
-    b: []
-};
+// 驼峰转连字符
+const hyphenateRE = /\B([A-Z])/g
+let cached = (str => {
+    return str.replace(hyphenateRE, '-$1').toLowerCase()
+})
+
+// 连字符转驼峰
+const camelizeRE = /-(\w)/g
+const camelize = (str) => {
+    return str.replace(camelizeRE, (_, c) => c ? c.toUpperCase() : '')
+}
+
+console.log(camelize("abc-aefg-igkLmn-是-dsfad"));
 
 
+// 正则表达日志
+// \B 是非单词边界 \b是单词边界（类似于空格 Tab 行首字 行尾 空行 标点）
